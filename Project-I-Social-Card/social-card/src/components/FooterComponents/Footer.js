30ca18fe76e5d305dd297dbbf0f89822e6c24220
justiny2c <justiny2c@gmail.com>
2019-05-27 23:01:43 -0700

    Calculator done. Needs adjusting to look perfect

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer () {
+    return (
+        <div class = "footer">
+            <p></p>
+        </div>
+    )
+}
+
+export default Footer
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay(){
+    return(
+        <div class = "calc-display">
+        <p>0</p>
+        </div>
+    )
+}
+
+export default CalculatorDisplay
