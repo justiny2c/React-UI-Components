1a733a15893ad9e99fe39d8c9705ef3a6745e7e4
justiny2c <justiny2c@gmail.com>
2019-05-27 21:40:49 -0700

    Finished with Social Card. Will start Footer (stretch)

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
 
-const Header = () = {
-return (
-    <div>
-    <ImageThumbnail />
-    <HeaderTitle />
-    <HeaderContent />
-    </div>
-)};
+function HeaderContainer(){
+    return (
+        <div class = "header-wrapper">
+            <div class = "block1">
+                <ImageThumbnail />
+            </div>
+            <div class = "block2">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    )
+};
+
+
+
+export default HeaderContainer
